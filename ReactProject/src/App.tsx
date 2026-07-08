05b4013e1c36976ae0a0c3643fff17825850fa08
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-07-08 22:04:04 +0200

    Merge

--- ReactProject/src/App.tsx
+++ ReactProject/src/App.tsx
@@ -1,17 +1,14 @@
 import './App.css'
-import Navbar from './Components/Text/Navbar/Navbar'
-import { Text } from './Components/Text/Text'
-// import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar'
+// import Footer from "./Components/Footer/Footer"
 
 
 function App() {
 
   return (
     <div>
-      <div>
-        <Navbar />
-      </div>
-      {/* <Footer /> */}
+      <Navbar />
+
     </div>
   )
 }
